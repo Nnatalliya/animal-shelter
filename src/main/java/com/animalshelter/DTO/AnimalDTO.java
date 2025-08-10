aa727b499efe3b2b6598ad4b8d6e9e3170a7eaea
Nnatalliya <116289279+Nnatalliya@users.noreply.github.com>
2025-08-10 10:49:54 +0300

    abv

--- src/main/java/com/animalshelter/DTO/AnimalDTO.java
+++ src/main/java/com/animalshelter/DTO/AnimalDTO.java
@@ -2,8 +2,10 @@ package com.animalshelter.DTO;
 
 import lombok.Builder;
 
+import java.io.Serializable;
+
 @Builder
-public class AnimalDTO {
+public class AnimalDTO implements Serializable {
     private String name;
     private String species;
     private Integer age;
